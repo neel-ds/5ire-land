5ab6b4b8e915656f73eb2f6f4e8081ac11a0319e
Neel <neelp0980@gmail.com>
2023-04-14 20:25:14 +0530

    Added landing page and components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "@/components/NavBar";
+import Footer from "../components/Footer";
+import { ChakraProvider } from "@chakra-ui/react";
+import { WagmiConfig } from "wagmi";
+import { client } from "../utils/wagmi";
+import { ConnectKitProvider } from "connectkit";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider theme="retro">
+          <Header />
+          <div className="min-h-[calc(100vh-68px)] pt-16 px-2 sm:px-4">
+            <ChakraProvider>
+              <Component {...pageProps} />
+            </ChakraProvider>
+          </div>
+          <Footer />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
+
+export default App;
